017b017b4ff009b481d1e1ce58c95304dc772c9a
antonio martinez <antonio.martinez13@g.austincc.edu>
2022-09-07 21:09:36 -0400

    login added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from '../account/login/login.component';
+import { RegisterComponent } from '../account/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
